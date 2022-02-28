fec9bc0606986153fd47d7414cbf43667c8a0731
Nguyen Trung HIeu <65000911+hieuntth2002015@users.noreply.github.com>
2022-02-28 22:43:56 +0700

    update
    1

--- EMI-Calculation/js/index.js
+++ EMI-Calculation/js/index.js
@@ -37,7 +37,26 @@ function drawChart() {
     var chart = new google.visualization.PieChart(oldChart);
     chart.draw(data, options);
 
-    var xmlHttpRequest = new xmlHttpRequest();
-    xmlHttpRequest.open('post', 'http://localhost:8080/v1/amounts', false);
-    xmlHttpRequest.send();
 }
+document.addEventListener('DOMContentLoaded', function () {
+
+    var tableBody = document.getElementById('my-table-data');
+    var xmlHttpRequest = new xmlHttpRequest();
+
+    xmlHttpRequest.onreadystatechange = function () {
+        if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
+            var data = JSON.parse(xmlHttpRequest.responseText);
+            var newContent = '';
+            for (let i = 0; i < data.length; i++) {
+                newContent += `<tr>
+                <td>${data[i].loan}</td>
+                <td>${data[i].interest}</td>
+                <td>${data[i].tenure}</td>
+                </tr>`;
+            }
+            tableBody.innerHTML = newContent;
+        }
+    }
+    xmlHttpRequest.open('get','http://localhost:8080/v1/amounts', false);
+    xmlHttpRequest.send();
+})
